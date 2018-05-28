55852589a090b3d878788f3f245b6383a7e8ffd7
Thomas Millet <thommil@pc67.home>
2018-05-28 16:32:28 +0200

    Config update

--- main.go
+++ main.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	"fmt"
-	"github.com/fvbock/endless"
-	"github.com/gin-gonic/gin"
-	"github.com/thommil/animals-go-auth/config"
 	"log"
 	"strings"
+
+	"github.com/fvbock/endless"
+	"github.com/gin-gonic/gin"
+	//"github.com/thommil/animals-go-auth/api"
+	"github.com/thommil/animals-go-common/config"
 )
 
-// Main of animals-go-auth
+// Configuration definition for animals-go-ws
+type Configuration struct {
+	HTTP struct {
+		Host string
+		Port int
+	}
+
+	Mongo struct {
+		URL string
+	}
+}
+
+// Main of animals-go-ws
 func main() {
 	//Config
-	config, err := config.LoadConfiguration()
+	configuration := &Configuration{}
+	err := config.LoadConfiguration("animals-go-auth", configuration)
 
 	if err != nil {
 		log.Fatal(err)
@@ -21,9 +36,11 @@ func main() {
 	//HTTP Server
 	router := gin.Default()
 
+	//Middlewares
+
 	//Start Server
 	var serverAddress strings.Builder
-	fmt.Fprintf(&serverAddress, "%s:%d", config.Http.Host, config.Http.Port)
+	fmt.Fprintf(&serverAddress, "%s:%d", configuration.HTTP.Host, configuration.HTTP.Port)
 	log.Printf("Starting HTTP server on %s\n", serverAddress.String())
 	log.Fatal(endless.ListenAndServe(serverAddress.String(), router))
 }
